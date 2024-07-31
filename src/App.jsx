3b66e3d0a64f565724e93ba0e16436c1884f2c2a
Manos Mantioglou <mmantioglou@avaca.gr>
2024-07-31 15:01:11 +0300

    styling the modal ui

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ function App() {
         });
     };
 
+    const handleCancelAddProject = () => {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+            };
+        });
+    };
+
     const handleAddProject = (projectData) => {
         setProjectsState((prevState) => {
             const newProject = {
@@ -34,12 +43,15 @@ function App() {
         });
     };
 
-    console.log(projectsState);
-
     let content;
 
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject onAdd={handleAddProject} />;
+        content = (
+            <NewProject
+                onAdd={handleAddProject}
+                onCancel={handleCancelAddProject}
+            />
+        );
     } else if (projectsState.selectedProjectId === undefined) {
         content = (
             <NoProjectSelected onStartAddProject={handleStartAddProject} />
